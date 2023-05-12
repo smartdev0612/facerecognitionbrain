c2e4771291991218f3ce0f37a96945454f37ac18
smartdev0612 <danieldev0612@gmail.com>
2023-05-11 21:12:57 -0700

    create image link form, add particles bg

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import './App.css'
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
+import ParticlesBg from 'particles-bg'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg type="cobweb" bg={true} />
       <Navigation />
       <Logo />
-      {/*<ImageLinkForm />
-      <FaceRecognition /> */}
+      <Rank />
+      <ImageLinkForm />
+      {/*<FaceRecognition /> */}
     </div>
   )
 }
